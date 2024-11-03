564af31996de455d77b5292a653d49d19e2831a3
Tenzin Tashi <Tenzin.Tashi@aexp.com>
2024-11-03 15:54:14 -0500

    release: checkstyle action

--- src/main/java/com/aexp/acq/go2/github_actions/CheckStyle.java
+++ src/main/java/com/aexp/acq/go2/github_actions/CheckStyle.java
@@ -2,30 +2,39 @@ package com.aexp.acq.go2.github_actions;
 
 import com.aexp.acq.go2.base.App;
 import com.aexp.acq.go2.base.BaseComponent;
+import com.aexp.acq.go2.base.RestResponse;
+import com.aexp.acq.go2.rest_interactions.CreatePullRequestReviewCommentEndPoint;
+import com.aexp.acq.go2.rest_interactions.ListPullRequestFilesEndPoint;
+import com.americanexpress.unify.jdocs.Document;
+import com.americanexpress.unify.jdocs.JDocument;
 import com.puppycrawl.tools.checkstyle.Checker;
 import com.puppycrawl.tools.checkstyle.ConfigurationLoader;
-import com.puppycrawl.tools.checkstyle.api.CheckstyleException;
+import com.puppycrawl.tools.checkstyle.PropertiesExpander;
+import com.puppycrawl.tools.checkstyle.api.AuditEvent;
+import com.puppycrawl.tools.checkstyle.api.AuditListener;
 import com.puppycrawl.tools.checkstyle.api.Configuration;
-import com.puppycrawl.tools.checkstyle.api.Violation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Properties;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.Map;
 
 public class CheckStyle extends BaseComponent {
 
   private static final Logger logger = LoggerFactory.getLogger(CheckStyle.class);
   private static final String checkStyleConfigUrl = App.instance().getProperty("checkstyle.config.url");
   private static final String checkStyleConfigPath = App.instance().getProperty("checkstyle.config.path");
+  private final List<String> modifiedFiles = new ArrayList<>();
+  private final Map<String, String> commitSHA = new HashMap<>();
 
   protected CheckStyle(String className) {
     super(className);
@@ -33,10 +42,9 @@ public class CheckStyle extends BaseComponent {
 
   @Override
   protected Object process(Object... vargs) {
-    // download the checkstyle config file
     try {
-      if(Files.notExists(Paths.get(checkStyleConfigPath))){
-        logger.info("Downloading the checkstyle config file from {}", checkStyleConfigUrl);
+      if (Files.notExists(Paths.get(checkStyleConfigPath))) {
+        logger.info("Checkstyle configuration file not found. Downloading from {}", checkStyleConfigUrl);
         try (InputStream inputStream = new URL(checkStyleConfigUrl).openStream()) {
           Files.copy(inputStream, Paths.get(checkStyleConfigPath), StandardCopyOption.REPLACE_EXISTING);
         }
@@ -44,63 +52,78 @@ public class CheckStyle extends BaseComponent {
           throw new RuntimeException(e);
         }
       }
-      Configuration config = ConfigurationLoader.loadConfiguration(checkStyleConfigPath,null, true);
+
+      invokeListPullRequestFilesEndPoint();
+      Checker checker = new Checker();
+      checker.setModuleClassLoader(Checker.class.getClassLoader());
+      Configuration configuration = ConfigurationLoader.loadConfiguration(checkStyleConfigPath, new PropertiesExpander(System.getProperties()));
+      checker.configure(configuration);
+      checker.addListener(new AuditListener() {
+        @Override
+        public void auditStarted(AuditEvent auditEvent) {
+
+        }
+
+        @Override
+        public void auditFinished(AuditEvent auditEvent) {
+
+        }
+
+        @Override
+        public void fileStarted(AuditEvent auditEvent) {
+
+        }
+
+        @Override
+        public void fileFinished(AuditEvent auditEvent) {
+
+        }
+
+        @Override
+        public void addError(AuditEvent auditEvent) {
+          String fileName = auditEvent.getFileName();
+          int line = auditEvent.getLine();
+          String errorMessage = auditEvent.getMessage();
+          String commitID = commitSHA.get(fileName);
+          invokeCreatePullRequestReviewCommentEndPoint(fileName, commitID, line, errorMessage);
+        }
+
+        @Override
+        public void addException(AuditEvent auditEvent, Throwable throwable) {
+
+        }
+      });
+      List<File> filesToCheck = modifiedFiles.stream().map(File::new).toList();
+      checker.process(filesToCheck);
+      checker.destroy();
     }
-    catch (RuntimeException e) {
-      throw new RuntimeException(e);
+    catch (Exception e) {
+      logger.error("Error while running checkstyle -> {}", e.getMessage());
     }
     return null;
   }
 
-  // TODO: implement the get changed files from a PR
-
-  private List<Violation> runCheckStyle(List<String> files) throws IOException, CheckstyleException {
-    List<Violation> violations = new ArrayList<>();
-
-    Configuration config = ConfigurationLoader.loadConfiguration(checkStyleConfigPath, new Properties);
-    Checker checker = new Checker();
-
-    checker.setModuleClassLoader(CheckStyle.class.getClassLoader());
-    checker.configure(config);
-
-    for (String file : files) {
-      checker.process(new File(file));
-    }
-    checker.destroy();
-
-    for (String file: files) {
-      violations.addAll()
-    }
-
-  }
-
-  private List<Violation> getViolationFromFile(String filePath) throws IOException {
-    List<Violation> violations = new ArrayList<>();
-    String command = "java -jar checkstyle-*.jar -c " +
-            checkStyleConfigPath + " " +
-            filePath;
-    ProcessBuilder processBuilder= new ProcessBuilder(command.split(" "));
-    processBuilder.redirectErrorStream(true);
-    Process process = processBuilder.start();
-
-    try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
-      String line;
-      Pattern violationPattern = Pattern.compile("^(.*):\\s*(\\d+)\\s*:.*?\\s*(.*)$");
-      while ((line = bufferedReader.readLine()) != null){
-        Matcher matcher = violationPattern.matcher(line);
-        if (matcher.matches()) {
-          String fileName = matcher.group(1);
-          int lineNumber = Integer.parseInt(matcher.group(2));
-          String message = matcher.group(3);
-          violations.add(new Violation(lineNumber, message));
-        }
+  private void invokeListPullRequestFilesEndPoint() {
+    ListPullRequestFilesEndPoint listPullRequestFilesEndPoint = new ListPullRequestFilesEndPoint("com.aexp.acq.go2.rest_interactions.ListPullRequestFilesEndPoint");
+    RestResponse restResponse = (RestResponse)listPullRequestFilesEndPoint.execute(App.instance().getProperty("github.base.url"),
+            App.instance().getProperty("github.repo"),
+            App.instance().getProperty("checkstyle.pull.request.number"));
+    // Parse the response
+    Document responseDoc = new JDocument(restResponse.getResponseBody());
+    for (int i = 0; i < responseDoc.getArraySize("$.[]"); i++) {
+      if (responseDoc.getString("$.[%].filename", String.valueOf(i)).endsWith(".java")) {
+        modifiedFiles.add(responseDoc.getString("$.[%].filename", String.valueOf(i)));
+        commitSHA.put(responseDoc.getString("$.[%].filename", String.valueOf(i)), responseDoc.getString("$.[%].sha", String.valueOf(i)));
       }
-    } catch (Exception e) {
-      e.printStackTrace();
     }
-    return violations;
   }
 
-
+  private void invokeCreatePullRequestReviewCommentEndPoint(String fileName, String commitSHA, int line, String errorMessage) {
+    CreatePullRequestReviewCommentEndPoint createPullRequestReviewCommentEndPoint = new CreatePullRequestReviewCommentEndPoint("com.aexp.acq.go2.rest_interactions.CreatePullRequestReviewCommentEndPoint");
+    createPullRequestReviewCommentEndPoint.execute(App.instance().getProperty("github.base.url"),
+            App.instance().getProperty("github.repo"),
+            App.instance().getProperty("checkstyle.pull.request.number"),
+            "{\"body\":\"" + errorMessage + "\",\"commit_id\":\"" + commitSHA + "\",\"path\":\"" + fileName + "\",\"line\":" + line + ", \"side\":\"RIGHT\"}");
+  }
 
 }
