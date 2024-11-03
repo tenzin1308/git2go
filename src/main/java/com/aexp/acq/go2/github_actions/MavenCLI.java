47bbfde04d1d85fe9cfa7694f60214e2ffc7ba45
Tenzin Tashi <Tenzin.Tashi@aexp.com>
2024-11-03 09:35:24 -0500

    release: maven cli action

--- src/main/java/com/aexp/acq/go2/github_actions/MavenCLI.java
+++ src/main/java/com/aexp/acq/go2/github_actions/MavenCLI.java
@@ -2,12 +2,14 @@ package com.aexp.acq.go2.github_actions;
 
 import com.aexp.acq.go2.base.App;
 import com.aexp.acq.go2.base.BaseComponent;
-import com.aexp.acq.go2.utils.BaseUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MavenCLI extends BaseComponent {
 
@@ -19,10 +21,11 @@ public class MavenCLI extends BaseComponent {
 
   @Override
   protected Object process(Object... vargs) {
-    String mavenCommand = App.instance().getProperty("phases");
-    if (BaseUtils.isNullOrEmpty(mavenCommand)) {
-      // TODO: Decide wheather to have validation here or in validator class
-    }
+    List<String> mavenCommand = new ArrayList<>();
+    mavenCommand.add("mvn");
+    Arrays.stream((App.instance().getProperty("phases")).split(","))
+            .map(String::trim)
+            .forEach(mavenCommand::add);
     try {
       boolean buildSuccess = runMavenCommand(mavenCommand);
       if (buildSuccess) {
@@ -38,15 +41,16 @@ public class MavenCLI extends BaseComponent {
     return null;
   }
 
-  private boolean runMavenCommand(String command) throws Exception {
-    Process process = Runtime.getRuntime().exec(command);
+  private boolean runMavenCommand(List<String> command) throws Exception {
+    ProcessBuilder processBuilder = new ProcessBuilder(command);
+    processBuilder.redirectErrorStream(true);
+    Process process = processBuilder.start();
     BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
     String line;
     while ((line = bufferedReader.readLine()) != null) {
       System.out.println(line);
     }
-    process.waitFor();
-    return process.exitValue() == 0;
+    return process.waitFor() == 0;
   }
 
 }
